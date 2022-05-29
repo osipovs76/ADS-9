e44aab6f433e5b76a029666c25c0f6f4d9fdb34e
Osipov Andrey(21-IST5) <94702769+osipovs76@users.noreply.github.com>
2022-05-29 19:10:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,26 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
+BST<std::string> warPeaceTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> textTree;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return warPeaceTree;
+  }
+  std::string text = "";
+  while (!file.eof()) {
+    text = "";
+    while (true) {
+      char sym = file.get();
+      if ((sym >= 65 && sym <= 90) || (sym >= 97 && sym <= 122))
+        text += tolower(sym);
+      else
+        break;
+    }
+    warPeaceTree.addNode(text);
+  }
+  file.close();
+  return warPeaceTree;
 }
